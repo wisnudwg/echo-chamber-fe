24b8f9863725f1b482c02238d20aabdc172e6877
wisnu-bdd <wisnu@super-staff.ai>
2025-11-20 22:32:17 +0700

    confirm axios approach works

--- src/hooks/use-open-ai.ts
+++ src/hooks/use-open-ai.ts
@@ -31,37 +31,6 @@ export function useOpenAI() {
       setMessages([...useOpenAIStore.getState().state.messages, r.message]);
       setTokenUsed(r.tokenUsed);
     },
-    // mutationFn: async (): Promise<OpenAIChatResponse> => {
-    //   const userInput: OpenAIChatMessage = {
-    //     role: "user",
-    //     content: useOpenAIStore.getState().state.input,
-    //   };
-
-    //   const response = await fetch(
-    //     "/api/open-ai",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({
-    //         messages: [...useOpenAIStore.getState().state.messages, userInput],
-    //       }),
-    //     },
-    //   );
-
-    //   if (!response.ok) {
-    //     throw new Error('Failed to get response from OpenAI');
-    //   };
-
-    //   return response.json();
-    // },
-    // onSuccess: (r) => {
-    //   setFinishReason(r.finishReason);
-    //   setInput("");
-    //   setMessages([...useOpenAIStore.getState().state.messages, r.message]);
-    //   setTokenUsed(r.tokenUsed);
-    // },
     onError: (e: any) => {
       console.error(e);
     },
